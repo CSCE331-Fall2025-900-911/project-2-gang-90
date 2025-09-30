9d6d4ab5f2bf98a8d6444beb9d918ce19a7906d5
sharksback <mrsharksback@tamu.edu>
2025-09-29 22:41:42 -0500

    added 3 more queries

--- backend/queries.sql
+++ backend/queries.sql
@@ -7,3 +7,18 @@ WHERE role = 'cashier';
 SELECT role, AVG(pay) AS average_salary
 FROM personnel
 GROUP BY role;
+
+--show menu items that cost less than $4.99
+SELECT item_name, price
+FROM menu
+WHERE price < 4.99;
+
+--Top 5 most popular menu items
+SELECT item_name, item_popularity
+FROM menu
+ORDER BY item_popularity DESC
+LIMIT 5;
+
+--Average price of all menu items
+SELECT AVG(price) AS average_item_price
+FROM menu;
